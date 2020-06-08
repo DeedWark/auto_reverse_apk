169f2536f7cc636bb34a7d68bd88ea21e20d40a5
DeedWark <deedwark@hotmail.fr>
2020-06-08 22:09:53 +0200

    change

--- reverse_apk_auto.sh
+++ reverse_apk_auto.sh
@@ -17,11 +17,10 @@ echo -e "Setup...\n"
 
 #check OS and package
 case $os in
-	[Dd]ebi* | [Uu]bun* | [Ll]inux[Mm]int* ) pkg="apt-get install" ; java="default-jre" ; no="-y" ; c_wget=$(dpkg -s wget 2>/dev/null) ; c_jre=$(dpkg -s default-jre 2>/dev/null);;
-	[Aa]rch* ) pkg="pacman -S" ; no="--noconfirm" ; java="jre-openjdk" ; c_wget=$(pacman -Qs wget 2>/dev/null) ; c_jre=$(pacman -Qs jre-openjdk 2>/dev/null);;
-	[Cc]ent* | [Ff]edo* | [Oo]l* | [Rr]ed[*Hh]* ) pkg="yum install" ; java="java-11-openjdk" ; no="-y" ; c_wget=$(yum list installed wget) ; c_jre=$(yum list installed java-11-openjdk);;
-	[Oo]pen[sS]* ) pkg="zypper install" ; no="-y" ; java="java-11-openjdk" ; c_wget=$(zypper se wget |grep -i "wget" 2>/dev/null) ; c_jre=$(zypper se java-11-openjdk |grep -i "java-11-openjdk" 2>/dev/null);;
-	* ) pkg="apk add";;
+	[Dd]ebi* | [Uu]bun* | [Ll]inux[Mm]int* ) pkg="apt-get install" ; java="default-jre" ; no="-y" ; c_curl=$(dpkg -s curl 2>/dev/null) ; c_jre=$(dpkg -s default-jre 2>/dev/null);;
+	[Aa]rch* ) pkg="pacman -S" ; no="--noconfirm" ; java="jre-openjdk" ; c_curl=$(pacman -Qs curl 2>/dev/null) ; c_jre=$(pacman -Qs jre-openjdk 2>/dev/null);;
+	[Cc]ent* | [Ff]edo* | [Oo]l* | [Rr]ed[*Hh]* ) pkg="yum install" ; java="java-11-openjdk" ; no="-y" ; c_curl=$(yum list installed curl) ; c_jre=$(yum list installed java-11-openjdk);;
+	[Oo]pen[sS]* ) pkg="zypper install" ; no="-y" ; java="java-11-openjdk" ; c_wget=$(zypper se curl |grep -i "wget" 2>/dev/null) ; c_jre=$(zypper se java-11-openjdk |grep -i "java-11-openjdk" 2>/dev/null);;
 esac
 
 #check wget package
@@ -29,7 +28,7 @@ if [ -z "$c_wget" ] ; then
 	echo 'wget is missing!'
 	read -r -p 'Do you want to install wget? [Y/N]' iwget
 	case $iwget in
-		[yYoO]* ) echo -e "Installing wget | Please wait!" ; $pkg $no wget 1>/dev/null;;
+		[yYoO]* ) echo -e "Installing wget | Please wait!" ; $pkg $no curl 1>/dev/null && echo -e "wget is now installed!\n";;
 		[nN]* ) echo "Try to install wget with '${pkg} wget'" ; exit 1;;
 	esac
 fi
@@ -39,7 +38,7 @@ if [ -z "$c_jre" ] ; then
 	echo 'JRE/Java package is missing!'
 	read -r -p 'Do you want to install JAVA? [Y/N]' ijre
 	case $ijre in
-		[YyOo]* ) echo -e "Installing default-jre | Please wait! (This could take a minute)" ; $pkg $no $java 1>/dev/null;;
+		[YyOo]* ) echo -e "Installing default-jre | Please wait! (This could take a minute)" ; $pkg $no $java 1>/dev/null && echo -e "$java is now installed!\n";;
 		[Nn]* ) echo "Try to install default-jre with '${pkg} ${java}'" ; exit 1;;
 	esac
 fi
@@ -55,9 +54,9 @@ else
 fi
 
 function help () {
-	echo -ne "This script will simplify your life by execute automaticaly
+	echo -ne "\nThis script will simplify your life by execute automaticaly
 	          a tool for reverse engineering Android APK files.
-		  To use it: ./script app.apk"
+		  To use it: ./script app.apk\n"
 	  }
 [[ $1 == "--help" || $1 == "-h" || $1 == "help" || -z $1 ]] && help && exit 0
 
