2be4ffe6483db0ea29aef2e05b4ea33464c0d8d3
DeedWark <deedwark@hotmail.fr>
2020-06-09 21:21:32 +0200

    setup

--- setup_reverse_apk.sh
+++ setup_reverse_apk.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
-#Make uncompil apk easier
+#Make disassembling apk easier
 #@DeedWark
 
 #check ROOT
 if [ $UID -ne 0 ] ; then
-	echo "You must launch it with sudo!"
+	echo "You must launch it with ROOT!"
 	exit 1
 fi
 
 #var
-os=$(grep -Ei "^ID=" /etc/os-release |cut -d '=' -f2)
-apktool="https://raw.githubusercontent.com/iBotPeaches/ApkTool/master/scripts/linux/apktool"
 apktool2="https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.4.1.jar"
+pkg="apt-get install"
+no="-y"
+java="default-jre"
+c_curl=$(dpkg -s curl 2>/dev/null)
+c_jre=$(dpkg -s default-jre 2>/dev/null)
 
 echo -e "APK Disassembler"
-
-#check OS and package
-pkg="" ; java="" ; no="" ; c_curl="" ; c_jre=""
-case $os in
-	[Dd]ebi* | [Uu]bun* | [Ll]inux[Mm]int* | [Kk]ali* ) pkg="apt-get install" ; java="default-jre" ; no="-y" ; c_curl=$(dpkg -s curl 2>/dev/null) ; c_jre=$(dpkg -s default-jre 2>/dev/null);;
-esac
 
 #check curl package
 if [ -z "$c_curl" ] ; then
@@ -30,11 +27,10 @@ if [ -z "$c_curl" ] ; then
 		[nN]* ) echo "Try to install wget with '${pkg} curl'" ; exit 1;;
 	esac
 fi
-#sleep 1
 #check java package
 if [ -z "$c_jre" ] ; then
 	echo 'Java package is missing!'
-	read -r -p 'Do you want to install JAVA? [Y/N] ' ijre
+	read -r -p 'Do you want to install Java? [Y/N] ' ijre
 	case $ijre in
 		[YyOo]* ) echo -e "Installing default-jre | Please wait! (This could take a minute)" ; $pkg $no $java 1>/dev/null && echo -e "Java is now installed!\n";;
 		[Nn]* ) echo "Try to install default-jre with '${pkg} ${java}'" ; exit 1;;
@@ -42,11 +38,8 @@ if [ -z "$c_jre" ] ; then
 fi
 #check & curl apktool jar && +x
 checkfile="/usr/local/bin/apktool"
-if [[ -f "$checkfile" ]]; then
-	echo ""
-else
+if [[ ! -f "$checkfile" ]]; then
 	mkdir ".apktool" 2>/dev/null && cd ".apktool" 2>/dev/null
-	echo -e "\nDownloading apktool ...\n" && curl -o apktool $apktool 1>/dev/null && chmod +x "apktool" && mv "apktool" "/usr/local/bin/"
 	echo -e "\nDownloading apktool.jar ...\n" && curl -o apktool.jar $apktool2 1>/dev/null && chmod +x "apktool.jar" && mv "apktool.jar" "/usr/local/bin/"
 	cd ".." && rm -rf ".apktool"
 fi
@@ -62,7 +55,7 @@ Use (dizapk -h / dizapk --help / dizapk help) to show this message\n"
 [[ $1 == "--help" || $1 == "-h" || $1 == "help" || -z $1 ]] && help && exit 0
 
 function diz () {
-	apktool d "$1"
+	java -jar /usr/local/bin/apktool.jar d "$1"
 	dir=$(echo -e "${1}" |cut -d '.' -f1)
 	echo -e "Disassembling done in ${dir} folder"
 }
@@ -75,4 +68,4 @@ if [[ $1 == "b" || $1 == "-b" ]] ; then
 else
 	diz
 fi
-###################################################
+####################### END #########################
